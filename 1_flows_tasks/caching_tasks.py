1dda13414b39fee141acfc688a5a79e0654f9192
Taylor Curran <taylorcurranagtc@gmail.com>
2023-12-27 19:17:13 -0700

    adding remote result storage

--- 1_flows_tasks/caching_tasks.py
+++ 1_flows_tasks/caching_tasks.py
@@ -1,6 +1,7 @@
 from prefect import flow, task
 from prefect.tasks import task_input_hash
 from datetime import timedelta
+from prefect_gcp.cloud_storage import GcsBucket
 
 # Task results can be cached
 # Provide a cache_key_fn AND/OR a cached_expiration timedelta to the task decorator
@@ -14,6 +15,17 @@ def hello_task(name_input):
 
 @flow
 def hello_flow(name_input):
+    hello_task(name_input)
+
+
+# Remote persistence of task results is recommended though optional
+# Use a prefect block to specify a GCS bucket to store results
+# Docs: https://docs.prefect.io/latest/concepts/results/#result-storage-location
+# Docs: https://prefecthq.github.io/prefect-gcp/blocks_catalog/#cloud-storage-module
+
+
+@flow(result_storage=GcsBucket(bucket="my-block-name"))
+def flow_with_remotely_persisted_results(name_input):
     hello_task(name_input)
 
 
