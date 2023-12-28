7e7c76e6a18dae2b5fbdc2ba47382e5706f47378
Taylor Curran <taylorcurranagtc@gmail.com>
2023-12-27 19:27:44 -0700

    adding subflow

--- 1_flows_tasks/basic_syntax.py
+++ 1_flows_tasks/basic_syntax.py
@@ -17,19 +17,24 @@ def fetch_weather(lat: float, lon: float):
     most_recent_temp = float(weather.json()["hourly"]["temperature_2m"][0])
     return most_recent_temp
 
-
+# Tasks can't run inside other tasks.
 @task
 def save_weather(temp: float):
     with open("weather.csv", "w+") as w:
         w.write(str(temp))
     return "Successfully wrote temp"
 
+# Flows CAN run inside other flows.
+@flow
+def my_subflow():
+    return "Flows can run inside other flows"
 
 @flow
 def pipeline(lat: float = 38.9, lon: float = -77.0):
     temp = fetch_weather(lat, lon)
     result = save_weather(temp)
-    return result
+    subflow_result = my_subflow()
+    return {"weather": result, "subflow": subflow_result}
 
 
 if __name__ == "__main__":
